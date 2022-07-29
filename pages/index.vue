4f525525ecf7fe9c5b9887d86e5913ea3ce356bb
jeroenverspeek <jeroen.verspeek@gmail.com>
2022-07-29 22:08:36 +0200

    add two buttons to start and stop cube

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1>Hexaturion</h1>
+  <div>
+    <h1>Hexaturion</h1>
+    <button @click="rubiks">Start rubiks</button>
+    <button @click="stop">Stop</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods: {
+    async rubiks() {
+      await this.$axios.get('http://192.168.178.35:3000/');
+    },
+    async stop () {
+      await this.$axios.get('http://192.168.178.35:3000/stop');
+    }
+  }
 }
 </script>
 
