8e298507c718027b38828c0f59e85a0cf2d600ac
Jeroen Verspeek <jeroen.verspeek@gmail.com>
2023-10-13 13:24:38 +0200

    add nRubiks

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,43 @@
-<template>
-  <div>
-    <h1>Hexaturion</h1>
-    <button @click="rubiks">Start rubiks</button>
-    <button @click="clock">Start clock</button>
-    <button @click="stop">Stop</button>
-  </div>
+<script setup>
+import { ref } from 'vue'
 
-</template>
+const nRubik = ref('')
 
-<script>
-export default {
-  name: 'IndexPage',
-  methods: {
-    async rubiks() {
-      await this.$axios.get('/start');
-    },
-    async clock() {
-      await this.$axios.get('/clock');
-    },
-    async stop () {
-      const response = await this.$axios.get('/stop');
-      console.log(response.data) 
-    }
-  }
+function rubiks() {
+  alert('Hi I am Rubik')
 }
+function clock() {
+  alert('Hi I am Clock')
+}
+function stop() {
+  alert('STOP!')
+}
+
 </script>
 
-<style scoped>
+<template>
+ 
+  <div>
+    <h1>Hexaturion</h1>
+    
+    <button @click="rubiks">Start rubiks</button><br>
+    <br>
 
-</style>
+    <span> nRubik: {{ nRubik }}</span><br> 
+    <br>
+
+    <div>nRubik:
+
+     <select v-model="nRubik">
+       <option disabled value="">Please select one</option>Property 'nRubik' does not exist on type '{ $: ComponentIntern
+       <option>2</option>
+       <option>3</option>
+       <option>4</option>
+       </select>
+    </div>
+    <h3>_______________</h3>
+    <button @click="clock">Start clock</button>
+    <h3>_______________</h3>
+    <button @click="stop">Stop</button>
+  </div>
+</template>
