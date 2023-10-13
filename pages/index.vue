e3450fb87d13b5fe7913f533178031fc932f92d8
Jeroen Verspeek <jeroen.verspeek@gmail.com>
2023-10-13 09:59:57 +0200

    added nRubik

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
+  <h1>Hexaturion</h1>
   <div>
-    <h1>Hexaturion</h1>
     <button @click="rubiks">Start rubiks</button>
+
+    <div>nRubik: {{ nRubik }}</div>
+    <select v-model="nRubik">
+      <option disabled value="">Dimension of Rubikscube</option>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+      <option>9</option>
+    </select>
+
+    <button @click="stop">Stop</button>
+
+  </div>
+  <div>
     <button @click="clock">Start clock</button>
     <button @click="stop">Stop</button>
   </div>
-
 </template>
 
 <script>
