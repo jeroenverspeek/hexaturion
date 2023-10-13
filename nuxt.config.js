b57869896c56940698169ded902072cae15f3f2a
Jeroen Verspeek <jeroen.verspeek@gmail.com>
2023-10-13 10:06:28 +0200

    testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: 'Led-cube frontend',
     htmlAttrs: {
       lang: 'en'
     },
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,16 @@
 <template>
-  <h1>Hexaturion</h1>
+  <h1>Hexaturion Cube Commander</h1>
   <div>
     <button @click="rubiks">Start rubiks</button>
-
+    <!--
     <div>nRubik: {{ nRubik }}</div>
     <select v-model="nRubik">
-      <option disabled value="">Dimension of Rubikscube</option>
-      <option>1</option>
+      <option disabled value="">nRubik</option>
       <option>2</option>
       <option>3</option>
       <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
     </select>
-
+    //-->
     <button @click="stop">Stop</button>
 
   </div>
