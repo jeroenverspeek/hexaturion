81c2b5087787c2bfa5ebeafcb6929c8bfe6c9e46
Jeroen Verspeek <jeroen.verspeek@gmail.com>
2023-10-15 12:12:19 +0200

    test cubeAppCommand

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@ export default {
 <template>
 
   <div>
-    <h1>Hexaturion version 0.99</h1>
+    <h1>Hexaturion version 0.1</h1>
     <!-- <span id="theTime"></span> -->
     <button @click="pseudoRubikscubeSolve">Start Rubik's cube pseudosolve</button><br>
     <br>
