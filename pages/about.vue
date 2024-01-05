2e4e551b940076fd9343f6a4c90ea4bb530c08e4
Jeroen Verspeek <jeroen.verspeek@gmail.com>
2024-01-05 19:37:55 +0100

    added <h4> tag

--- pages/about.vue
+++ pages/about.vue
@@ -10,10 +10,7 @@ const { version } = useConfig();
     <h1 class="title">About Hexaturion</h1>
     <h2 class="subtitle">version {{version}}</h2>
     <h3>Commander of the six faces</h3><br>
-
     <h4>
-    A graphical user interface for the led-hexahedron<br><br><br>
-
     <div>
       <img src="/gallery/cube_rubikscube_pattern.jpg" alt="image not found" width="300px" height="300px" />
     </div>
