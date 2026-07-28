ce78ce8c36c130486146dcb6f513880b97b343d3
jeroenverspeek <jeroen.verspeek@gmail.com>
2026-07-28 10:13:21 +0200

    added cube picture to README.md

--- README.md
+++ README.md
@@ -1,71 +1,22 @@
 # Hexaturion
 
-Hexaturion offers a graphical user interface for controlling the LED cube (led-hexahedron).
+Hexaturion offers a graphical user interface for controlling the LED cube
+(led-hexahedron).
 
 ## Commander of the six faces
 
-The name Hexaturion is chosen in analogy with the Roman centurion (commander of 100 soldiers) and decurion (commander of 10 soldiers).
+The name Hexaturion is chosen in analogy with the Roman centurion
+(commander of 100 soldiers) and decurion (commander of 10 soldiers).
+
+![Hexaturion cube](public/gallery/cube_rubikscube_pattern.jpg)
 
 ## Software platform
 
-It is written in VUE-3, in typescript and javascript
+It is written in Vue 3, in TypeScript and JavaScript.
 
 ## How to use
 
-Scan the QR-code that is shown on startup of the cube, or manually start a client in a webbrowser:
+Scan the QR-code that is shown on startup of the cube, or manually start a
+client in a web browser:
 
-http://hexaturion.com
-
-On the raspberry pi a server is automatically run at startup.  
-
-The IP address of the raspberry pi is fixed in the Hexaturion software to be 192.168.1.136. 
-
-If needed, you can manually start the server from the command line:
-
-    ssh <username>@192.168.1.136
-    cd $lebcube
-    sudo node server.js
-
-Raspberrypi listening to Hexaturion on [port 3000]http://localhost:3000
-
-## For development
-
-Install nodemon on the raspberry pi. The nodemon command simplifies the development process by automatically reloading the application (restarting the server) whenever you make changes to the code.
-
-    sudo npm install -g nodemon
-
-Start the server:
-
-    sudo nodemon server.js
-
-this gives the message:
-
-Listening on http://localhost:3000/
-
-To open the Hexaturion GUI start http://localhost:3000 in a webbrowser.
-
-For debugging use the right mouse button -> Inspect -> Console
-
-## Testing Hexaturion on your computer
-
-Clone repository to you computer:
-
-    cd {$GITDIR}/hexaturion
-    git clone git@github.com:jeroenverspeek/hexaturion.git
-
-Install all node-modules:
-
-    npm install
-
-Set up a local development server
-
-    npm run dev
-
-this gives the message:
-
-Listening on http://localhost:3000/
-
-To open the Hexaturion GUI start  http://localhost:3000 in a webbrowser.
-
-Note that https://hexaturion.com is not suited for testing purposes, as committed changes will take some time to be reflected therein.
-
+[http://hexaturion.com](http://hexaturion.com)
--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,108 @@
-
 <script setup lang="ts">
-const currentTab = ref('clock');
+const currentTab = ref("clock");
 </script>
 
 <template>
   <div class="box">
-    <h1 class="title">Hexaturion</h1>
     <hr />
     <div class="tabs is-boxed">
       <ol>
         <ul>
           <li :class="{ 'is-active': currentTab === 'clock' }">
             <a @click="currentTab = 'clock'">
-              <span class="icon is-small"><i class="fa-solid fa-clock"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-clock"></i
+              ></span>
               <span>Clock</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'video' }">
             <a @click="currentTab = 'video'">
-              <span class="icon is-small"><i class="fa-solid fa-video"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-video"></i
+              ></span>
               <span>Video</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'pictures' }">
             <a @click="currentTab = 'pictures'">
-              <span class="icon is-small"><i class="fa-solid fa-image"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-image"></i
+              ></span>
               <span>Pictures</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'rubiksCube' }">
             <a @click="currentTab = 'rubiksCube'">
-              <span class="icon is-small"><i class="fa-solid fa-cube"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-cube"></i
+              ></span>
               <span>Rubik's cube</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'slidingPuzzle' }">
             <a @click="currentTab = 'slidingPuzzle'">
-              <span class="icon is-small"><i class="fa-solid fa-cube"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-cube"></i
+              ></span>
               <span>Sliding puzzle</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'celestialBodies' }">
             <a @click="currentTab = 'celestialBodies'">
-              <span class="icon is-small"><i class="fa-solid fa-earth-europe"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-earth-europe"></i
+              ></span>
               <span>Celestial bodies</span>
             </a>
           </li>
-          </ul>
-          <ul>
+        </ul>
+        <ul>
           <li :class="{ 'is-active': currentTab === 'stockMarketQuotes' }">
             <a @click="currentTab = 'stockMarketQuotes'">
-              <span class="icon is-small"><i class="fa-solid fa-money-bill-trend-up"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-money-bill-trend-up"></i
+              ></span>
               <span>Stock market</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'helloWorld' }">
             <a @click="currentTab = 'helloWorld'">
-              <span class="icon is-small"><i class="fa-solid fa-door-open"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-door-open"></i
+              ></span>
               <span>Hello world</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'demo' }">
             <a @click="currentTab = 'demo'">
-              <span class="icon is-small"><i class="fa-solid fa-cube"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-cube"></i
+              ></span>
               <span>Demo</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'fireworks' }">
             <a @click="currentTab = 'fireworks'">
-              <span class="icon is-small"><i class="fa-solid fa-wand-magic-sparkles"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-wand-magic-sparkles"></i
+              ></span>
               <span>Fireworks</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'superDemo' }">
             <a @click="currentTab = 'superDemo'">
-              <span class="icon is-small"><i class="fa-solid fa-cube"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-cube"></i
+              ></span>
               <span>Super demo</span>
             </a>
           </li>
           <li :class="{ 'is-active': currentTab === 'boss' }">
             <a @click="currentTab = 'boss'">
-              <span class="icon is-small"><i class="fa-solid fa-cube"></i></span>
+              <span class="icon is-small"
+                ><i class="fa-solid fa-cube"></i
+              ></span>
               <span>BOSS</span>
             </a>
           </li>
